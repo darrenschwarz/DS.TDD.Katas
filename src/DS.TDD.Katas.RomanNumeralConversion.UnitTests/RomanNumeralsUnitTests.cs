760e32129cbbd55f19b554e8a84a9cb619847223
Darren Schwarz <darren.schwarz@flagstoneim.com>
2022-02-10 17:10:07 +0000

    Refactored to generalise the  approach.

--- src/DS.TDD.Katas.RomanNumeralConversion.UnitTests/RomanNumeralsUnitTests.cs
+++ src/DS.TDD.Katas.RomanNumeralConversion.UnitTests/RomanNumeralsUnitTests.cs
@@ -9,6 +9,12 @@ namespace DS.TDD.Katas.RomanNumeralConversion.UnitTests
         [InlineData(2, "II")]
         [InlineData(3, "III")]
         [InlineData(4, "IV")]
+        [InlineData(5, "V")]
+        [InlineData(6, "VI")]
+        [InlineData(7, "VII")]
+        [InlineData(8, "VIII")]
+        [InlineData(9, "IX")]
+        [InlineData(10, "X")]
         public void IntegerIsConvertedToRomanNumerals(int integer, string romanNumerals)
         {
             Assert.True(romanNumerals.Equals(RomanNumerals.FromInteger(integer)));
--- src/DS.TDD.Katas.RomanNumeralConversion/RomanNumerals.cs
+++ src/DS.TDD.Katas.RomanNumeralConversion/RomanNumerals.cs
@@ -5,23 +5,35 @@ namespace DS.TDD.Katas.RomanNumeralConversion
 {
     public class RomanNumerals
     {
+        private static List<int> _integers = new List<int>(){10, 9, 5, 4, 1};
+        private static List<string> _romanNumerals = new List<string>() { "X", "IX", "V", "IV", "I" };
+
         public static string FromInteger(int integer)
         {
             var converted = new StringBuilder();
             var integerToConvert = integer;
 
-            if (integerToConvert >= 4)
+            for (int i = 0; i < _integers.Count; i++)
             {
-                converted.Append("IV");
-                integerToConvert -= 4;
+                integerToConvert = AppendToConverted(converted, integerToConvert, _integers[i], _romanNumerals[i]);
             }
 
-            for (int i = 0; i < integerToConvert; i++)
-            {
-                converted.Append("I");
-            }
-            
             return converted.ToString() ;
         }
+
+        public static int AppendToConverted(StringBuilder converted, int integerToConvert, int integer, string romanNumeral)
+        {
+            var currentIntegerToConvert = integerToConvert;
+
+            while (currentIntegerToConvert >= integer)
+            {
+                converted.Append(romanNumeral);
+                currentIntegerToConvert -= integer;
+            }
+
+            return currentIntegerToConvert;
+        }
+
+        
     }
 }
