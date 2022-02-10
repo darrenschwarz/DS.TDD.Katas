e6f0accc9330d18a1be74a0bb4b3f9fddf0ca9eb
Darren Schwarz <darrenschwarz@gmail.com>
2022-02-10 08:15:57 +0000

    Failing test on converting 3.

--- src/DS.TDD.Katas.RomanNumeralConversion.UnitTests/RomanNumeralsUnitTests.cs
+++ src/DS.TDD.Katas.RomanNumeralConversion.UnitTests/RomanNumeralsUnitTests.cs
@@ -7,6 +7,7 @@ namespace DS.TDD.Katas.RomanNumeralConversion.UnitTests
         [Theory]
         [InlineData(1, "I")]
         [InlineData(2, "II")]
+        [InlineData(3, "III")]
         public void IntegerIsConvertedToRomanNumerals(int integer, string romanNumerals)
         {
             Assert.True(romanNumerals.Equals(RomanNumerals.FromInteger(integer)));
